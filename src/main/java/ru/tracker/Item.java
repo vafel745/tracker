afd8e118f9831abcef8bae7d5435e591ffbe850c
vafel745 <vaflvafl47@gmail.com>
2025-10-30 16:16:31 +0300

    7.9 Перегрузка конструктора

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -4,6 +4,19 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +31,11 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item("Ball");
+        Item item2 = new Item(1, "Car");
     }
 }
