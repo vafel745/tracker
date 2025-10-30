d2d7be85e5d8c4248997740c0dbe6f164b0b9f0c
vafel745 <vaflvafl47@gmail.com>
2025-10-30 16:07:47 +0300

    7.8 Конструктор

---
+++ src/main/java/ru/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Тип ошибки: " + active);
+        System.out.println("Статус ошибки: " + status);
+        System.out.println("Сообщение о ошибке: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 1, "ArrayIndexOutOfBoundsException");
+        Error error2 = new Error(true, 2, "ArithmeticException");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
