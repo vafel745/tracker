b19c8770b5fed666424dfefd47a3ed797ef7c978
vafel745 <vaflvafl47@gmail.com>
2025-11-20 13:54:36 +0300

    7.14 Рефакторинг. Площадь треугольника

--- src/test/java/ru/oop/TriangleTest.java
+++ src/test/java/ru/oop/TriangleTest.java
@@ -15,4 +15,15 @@ public class TriangleTest {
         double expected = 8;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
+
+    @Test
+    public void when40and40and40ThenMinus1() {
+        Point a = new Point(4, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(4, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
 }
