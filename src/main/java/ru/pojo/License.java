c6f05c0a77e9d9d4f1728d34eff52ee03dfb6a5b
vafel745 <vaflvafl47@gmail.com>
2026-02-13 15:36:27 +0300

    9.6 Сравнение моделей. Метод equals

--- src/main/java/ru/pojo/License.java
+++ src/main/java/ru/pojo/License.java
@@ -43,6 +43,9 @@ public class License {
 
     @Override
     public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
