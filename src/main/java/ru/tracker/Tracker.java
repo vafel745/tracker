766f2b3ca63b7a327f6cda9bc19a2add7d2121e4
vafel745 <vaflvafl47@gmail.com>
2026-03-10 11:45:22 +0300

    9.8 Метод замены заявки. Tracker.replace

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -50,7 +50,11 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        items[index] = item;
-        return true;
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 }
