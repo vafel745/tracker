e2695276ac29bd748d8552bb86e35b0273a99879
vafel745 <vaflvafl47@gmail.com>
2026-04-10 19:14:26 +0300

    10.2 Класс Scanner

---
+++ src/main/java/ru/io/MagicBall.java
@@ -0,0 +1,19 @@
+package ru.io;
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("да");
+        } else if (answer == 1) {
+            System.out.println("нет");
+        } else {
+            System.out.println("Может быть");
+        }
+    }
+}
