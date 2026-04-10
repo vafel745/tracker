8d4e575134592953bac2bcaa221f96b70fa76448
vafel745 <vaflvafl47@gmail.com>
2026-04-10 19:57:16 +0300

    10.2 Класс Scanner

--- src/main/java/ru/io/MagicBall.java
+++ src/main/java/ru/io/MagicBall.java
@@ -9,9 +9,9 @@ public class MagicBall {
         input.nextLine();
         int answer = new Random().nextInt(3);
         if (answer == 0) {
-            System.out.println("да");
+            System.out.println("Да");
         } else if (answer == 1) {
-            System.out.println("нет");
+            System.out.println("Нет");
         } else {
             System.out.println("Может быть");
         }
