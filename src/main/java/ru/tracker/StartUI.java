db31a2ab9dd460498cc6d2a55550a4b1ad49d326
vafel745 <vaflvafl47@gmail.com>
2026-06-03 16:07:17 +0300

    10.4 StartUI. Вывод меню.

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -1,13 +1,37 @@
 package ru.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
-public class StartUI extends Item {
+public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени", "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormat);
-        System.out.println(item);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
