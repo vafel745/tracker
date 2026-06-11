df3b3d2daeb8f45fba572f81b96eae5d8f6c2666
vafel745 <vaflvafl47@gmail.com>
2026-06-11 19:44:14 +0300

    11.3 throw new RuntimeException.docx

---
+++ src/main/java/ru/ex/Factorial.java
@@ -0,0 +1,18 @@
+package ru.ex;
+
+public class Factorial {
+    public int calc(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("Факториал не может быть отрицательным");
+        }
+        int result = 1;
+        for (int index = 2; index <= number; index++) {
+            result *= index;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new Factorial().calc(-1);
+    }
+}
