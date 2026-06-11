0f583d3221c2b52fce333642ed192bf5454a7113
vafel745 <vaflvafl47@gmail.com>
2026-06-11 20:32:30 +0300

    11.6 Иерархия исключений.docx

---
+++ src/main/java/ru/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.ex;
+
+import java.util.Objects;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        int lengthArray = users.length - 1;
+        for (int i = 0; i <= lengthArray; i++) {
+            if (Objects.equals(users[i].getUsername(), login)) {
+                return users[i];
+            } else {
+                throw new UserNotFoundException("User not found");
+            }
+        }
+        return null;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUsername().length() < 3) {
+            throw new UserInvalidException("The user is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Ivan Ivanov", true)
+        };
+        try {
+            User user = findUser(users, "Ivan Ivano");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ei) {
+            ei.printStackTrace();
+        } catch (UserNotFoundException enf) {
+            enf.printStackTrace();
+        }
+    }
+}
