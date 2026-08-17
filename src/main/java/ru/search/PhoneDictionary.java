afb2d5748817fc4c487834a78aca34f2f338aa24
vafel745 <vaflvafl47@gmail.com>
2026-08-17 16:41:05 +0300

    13.4 Телефонный справочник

--- src/main/java/ru/search/PhoneDictionary.java
+++ src/main/java/ru/search/PhoneDictionary.java
@@ -18,7 +18,7 @@ public class PhoneDictionary {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key) || person.getAddress().contains(key)) {
-                result.addAll(persons);
+                result.add(person);
             }
         }
         return result;
